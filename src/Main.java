2eb4c16a88c0ae37447b71876bad41c4a8db9277
Tahir Aslan <tahiraslan@ymail.com>
2022-07-25 10:16:23 +0300

    tahir aslan

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
-    public static void Main(String[] args){ //programın ana metodu
-        System.out.println();
+    public static void main(String[] args){ //programın ana metodu
+        System.out.println("Merhaba Java");
+        System.out.print("Tahir ASLAN");
+        System.out.print("Erhan Kaya ile Java Dersleri");
+        System.out.print("İlk Ders");
+
+        //
+
 
 
     }
