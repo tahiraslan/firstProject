2ed54abcab267bc5e39b32ca47f3db6b656ed9f4
Tahir Aslan <tahiraslan@ymail.com>
2022-07-25 10:36:47 +0300

    tahir aslan

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,26 @@
 public class Main {
     public static void main(String[] args){ //programın ana metodu
         System.out.println("Merhaba Java");
-        System.out.print("Tahir ASLAN");
-        System.out.print("Erhan Kaya ile Java Dersleri");
-        System.out.print("İlk Ders");
+        System.out.println("Tahir ASLAN");
+        System.out.println("Erhan Kaya ile Java Dersleri");
+        System.out.println("İlk Ders");
 
         //Değişkenler
 
         String isim= "Tahir\nASLAN";
         char karakter='F';
+        byte sayi2=2;
         int sayi1=15;
+        short sayi3=155;
+        long sayi4=1514; //-9223372036854775808
+        double kesirli1=15;
+        float kesirli2=12.9f;
+
+        boolean dogruyanlis=true; //false
+
+
+        System.out.println(isim);
+
 
 
 
